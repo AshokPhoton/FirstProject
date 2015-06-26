4c6ddec27ae9204a5028137b8a89bf5d60c399ba
Ashok Kumar <ashokkumar@IOS-App.local>
2015-06-26 19:12:26 +0530

    Add the main project

---
+++ MainProject/MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by Ashok Kumar on 26/06/15.
+//  Copyright (c) 2015 Ashok Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by Ashok Kumar on 26/06/15.
+//  Copyright (c) 2015 Ashok Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
